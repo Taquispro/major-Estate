156188606b07973a656cf6259370e7389bb65976
Taquispro <121485751+Taquispro@users.noreply.github.com>
2025-01-14 01:43:14 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -15,3 +16,11 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is runnig at port 3000");
 });
+//req ;- from cient to server
+//res ;- from server to client
+//after creating user.route.js file we can use app.use()
+// app.get("/test", (req, res) => {
+//   res.send("Hello world");
+// });
+app.use("/api/user", userRouter);
+//it will give the response to api/user/test
