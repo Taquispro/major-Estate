6e189db151da2a4c8b3095ebaf05dad0c89c2e6c
Taquispro <121485751+Taquispro@users.noreply.github.com>
2025-02-10 22:04:05 +0530

    Add google OAuth functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import OAuth from "../components/OAuth";
 //useNavigate is to redirect the page
 
 function Signup() {
@@ -70,6 +71,7 @@ function Signup() {
         >
           {loading ? "Loading..." : "Sign Up"}
         </button>
+        <OAuth />
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
