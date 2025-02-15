e3df18ebc55657defbcb4fa9b07471aafcc39e51
Taquispro <121485751+Taquispro@users.noreply.github.com>
2025-02-16 03:19:15 +0530

    create update listing API route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -2,9 +2,11 @@ import express from "express";
 import {
   createlisting,
   deleteListing,
+  updateListing,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 router.post("/create", verifyToken, createlisting);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
 export default router;
