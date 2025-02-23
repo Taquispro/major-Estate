4dea214a8b2ec94a049ea30b1fb6de8a7e55abc5
Taquispro <121485751+Taquispro@users.noreply.github.com>
2025-02-23 17:12:31 +0530

    Complete listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,16 +1,28 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Keyboard } from "swiper/modules"; // Correct import
 import "swiper/css/bundle";
 import SwiperCore from "swiper";
+import {
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkerAlt,
+  FaParking,
+  FaShare,
+} from "react-icons/fa";
 
 function Listing() {
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const [contact, setContact] = useState(false);
+  const { currentUser } = useSelector((state) => state.user);
   console.log(listing);
   SwiperCore.use(Navigation);
   useEffect(() => {
@@ -27,6 +39,7 @@ function Listing() {
         setListing(data);
         setLoading(false);
         setError(false);
+        // eslint-disable-next-line no-unused-vars
       } catch (error) {
         setError(true);
         setLoading(false);
@@ -41,25 +54,102 @@ function Listing() {
         <p className="text-center my-7 text-wxl">Something went wrong!</p>
       )}
       {listing && !loading && !error && (
-        <Swiper
-          navigation
-          keyboard={{ enabled: true }} // Enable arrow key navigation
-          modules={[Navigation, Keyboard]} // Use both Navigation & Keyboard
-          className="w-full h-[60vh] sm:h-[40vh] md:h-[50vh] lg:h-[60vh]"
-        >
-          {listing.imageUrls.map((url) => (
-            <SwiperSlide
-              key={url}
-              className="flex justify-center items-center bg-black"
-            >
-              <img
-                src={url}
-                alt="Listing"
-                className="w-full h-full object-contain"
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        <>
+          <Swiper
+            navigation
+            keyboard={{ enabled: true }} // Enable arrow key navigation
+            modules={[Navigation, Keyboard]} // Use both Navigation & Keyboard
+            className="w-full h-[60vh] sm:h-[40vh] md:h-[50vh] lg:h-[60vh]"
+          >
+            {listing.imageUrls.map((url) => (
+              <SwiperSlide
+                key={url}
+                className="flex justify-center items-center bg-black"
+              >
+                <img
+                  src={url}
+                  alt="Listing"
+                  className="w-full h-full object-contain"
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
+            <FaShare
+              className="text-slate-500"
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                setCopied(true);
+                setTimeout(() => {
+                  setCopied(false);
+                }, 2000);
+              }}
+            />
+          </div>
+          {copied && (
+            <p className="fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2">
+              Link copied!
+            </p>
+          )}
+          <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
+            <p className="text-2xl font-semibold">
+              {listing.name} - ${" "}
+              {listing.offer
+                ? listing.discountPrice.toLocaleString("en-US")
+                : listing.regularPrice.toLocaleString("en-US")}
+              {listing.type === "rent" && " / month"}
+            </p>
+            <p className="flex items-center mt-6 gap-2 text-slate-600  text-sm">
+              <FaMapMarkerAlt className="text-green-700" />
+              {listing.address}
+            </p>
+            <div className="flex gap-4">
+              <p className="bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                {listing.type === "rent" ? "For Rent" : "For Sale"}
+              </p>
+              {listing.offer && (
+                <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                  ${+listing.regularPrice - +listing.discountPrice} OFF
+                </p>
+              )}
+            </div>
+            <p className="text-slate-800">
+              <span className="font-semibold text-black">Description - </span>
+              {listing.description}
+            </p>
+            <ul className="text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6">
+              <li className="flex items-center gap-1 whitespace-nowrap ">
+                <FaBed className="text-lg" />
+                {listing.bedrooms > 1
+                  ? `${listing.bedrooms} beds `
+                  : `${listing.bedrooms} bed `}
+              </li>
+              <li className="flex items-center gap-1 whitespace-nowrap ">
+                <FaBath className="text-lg" />
+                {listing.bathrooms > 1
+                  ? `${listing.bathrooms} baths `
+                  : `${listing.bathrooms} bath `}
+              </li>
+              <li className="flex items-center gap-1 whitespace-nowrap ">
+                <FaParking className="text-lg" />
+                {listing.parking ? "Parking spot" : "No Parking"}
+              </li>
+              <li className="flex items-center gap-1 whitespace-nowrap ">
+                <FaChair className="text-lg" />
+                {listing.furnished ? "Furnished" : "Unfurnished"}
+              </li>
+            </ul>
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button
+                onClick={() => setContact(true)}
+                className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3"
+              >
+                Contact landlord
+              </button>
+            )}
+            {contact && <Contact listing={listing} />}
+          </div>
+        </>
       )}
     </main>
   );
